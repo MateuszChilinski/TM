85608fe897e9c01e88ad0021e09825a99d6c9f0b
Mateusz Chiliński <mateusz@chilinski.eu>
2019-04-09 23:40:17 +0200

    first commit

---
+++ TMStats/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMStats
+{
+    class Program
+    {
+        static public List<Visit> LoadJson()
+        {
+            using (StreamReader r = new StreamReader("visits_1000.txt"))
+            {
+                string json = r.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<Visit>>(json);
+                return items;
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            var visits = LoadJson();
+            var file = File.AppendText("results.csv");
+            visits.Where(x => x.Text.Length > 3).GroupBy(x => x.Code)
+                .Select(y => new { Code = y.Key, Count = y.Count() }).OrderByDescending(x => x.Count).ToList()
+                .ForEach(x => file.WriteLine(x.Code + "," + x.Count));
+        }
+    }
+    public class Visit
+    {
+        public string Text;
+        public string Sex;
+        public int Age;
+        public string Code;
+    }
+}
