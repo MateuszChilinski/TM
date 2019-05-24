3fc1ffbc71d8eb38cf13031e10b86863c7093f2f
Mateusz Chiliński <mateusz@chilinski.eu>
2019-05-24 16:28:04 +0200

    update

---
+++ ELMO2TensorFlow/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELMO2TensorFlow
+{
+    class Tensor
+    {
+        public string Name { get; set; }
+        public double[] Vector { get; set; }
+
+        public Tensor()
+        {
+
+        }
+        public Tensor(string name, double[] vector)
+        {
+            Name = name;
+            Vector = vector;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tensor> tensors = new List<Tensor>();
+            foreach (string line in File.ReadAllLines("leekseek.txt"))
+            {
+                if (line == "") continue;
+                var lineSplit = line.Split('	');
+                var name = lineSplit[0];
+                if(name.Contains(".") || name.Contains("�") || name.Contains(":") || name.Contains(",")) continue;
+                double[] vector = Array.ConvertAll(lineSplit.Skip(1).ToArray(), double.Parse);
+                tensors.Add(new Tensor(name, vector));
+            }
+            List<Tensor> uniqueTensors = new List<Tensor>();
+            foreach (var group in tensors.GroupBy(x => x.Name))
+            {
+                Tensor t = new Tensor();
+                t.Name = group.Key;
+                t.Vector = new double[group.FirstOrDefault().Vector.Length];
+                foreach (var item in group)
+                {
+                    for (int i = 0; i < item.Vector.Length; i++)
+                    {
+                        t.Vector[i] += item.Vector[i] / group.Count();
+                    }
+                }
+                uniqueTensors.Add(t);
+            }
+
+            tensors = uniqueTensors; // delete if not avg
+
+            if (File.Exists("vectors.tsv"))
+            {
+                File.Delete("vectors.tsv");
+            }
+
+            // Create a new file     
+            using (StreamWriter sw = File.CreateText("vectors.tsv"))
+            {
+                foreach (double[] vector in tensors.Select(x => x.Vector))
+                {
+                    string line = "";
+                    foreach (var scalar in vector)
+                        line += scalar + "\t";
+                    line = line.Substring(0, line.Length - 2);
+                    sw.WriteLine(line);
+                }
+            }
+            if (File.Exists("labels.tsv"))
+            {
+                File.Delete("labels.tsv");
+            }
+
+            // Create a new file     
+            using (StreamWriter sw = File.CreateText("labels.tsv"))
+            {
+                foreach (string name in tensors.Select(x => x.Name))
+                {
+                    string line = name;
+                    sw.WriteLine(line);
+                }
+            }
+        }
+    }
+}
--- TMStats/Program.cs
+++ TMStats/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WordCloudGen = WordCloud.WordCloud;
 
 namespace TMStats
 {
@@ -27,6 +30,75 @@ namespace TMStats
             visits.Where(x => x.Text.Length > 3).GroupBy(x => x.Code)
                 .Select(y => new { Code = y.Key, Count = y.Count() }).OrderByDescending(x => x.Count).ToList()
                 .ForEach(x => file.WriteLine(x.Code + "," + x.Count));
+            var visits_parsed = visits.Where(x => x.Text.Length > 3);
+            var keys = visits.Where(x => x.Text.Length > 3).GroupBy(x => x.Code)
+                .Select(y => new {Code = y.Key, Count = y.Count()}).OrderByDescending(x => x.Count).Where(x => x.Count >= 10).Select(x => x.Code);
+                Dictionary<string, int> wordFreq = new Dictionary<string, int>();
+                foreach (var visit in visits_parsed)
+                {
+                    visit.Text = visit.Text.Replace(".", " .");
+                    visit.Text = visit.Text.Replace(":", "  :");
+                    visit.Text = visit.Text.Replace(",", " , ");
+                    visit.Text = visit.Text.Replace("(", " ( ");
+                    visit.Text = visit.Text.Replace(")", " )");
+                    visit.Text = visit.Text.Replace("  ", " ");
+                }
+                foreach (var visit in visits_parsed)
+                {
+                    var words = visit.Text.Split(' ');
+                    foreach (var word in words)
+                    {
+                        if (wordFreq.ContainsKey(word))
+                            wordFreq[word]++;
+                        else
+                            wordFreq.Add(word, 1);
+                    }
+                }
+            var vocabulary = File.AppendText("vocabulary");
+            var train = File.AppendText("train");
+
+            var ordrred = wordFreq.OrderByDescending(x => x.Value);
+            vocabulary.WriteLine("</S>");
+            vocabulary.WriteLine("<S>");
+            vocabulary.WriteLine("<UNK>");
+            foreach (var word in ordrred)
+            {
+                vocabulary.WriteLine(word.Key);
+            }
+            foreach (var visit in visits_parsed)
+            {
+                train.WriteLine(visit.Text);
+            }
+
+            //var wc = new WordCloudGen(1200, 1200);
+            //List<string> wordsCloud = new List<string>();
+            //List<int> wordsFreq = new List<int>();
+            //int countAll = 0;
+            //foreach (var entity in wordFreq)
+            //{
+            //    countAll += entity.Value;
+            //}
+
+            //List<string> toRemove = new List<string>();
+            //foreach (var entity in wordFreq)
+            //{
+            //    if (entity.Value < countAll * 0.005)
+            //    {
+            //        toRemove.Add(entity.Key);
+            //    }
+            //}
+            //foreach (var remov in toRemove)
+            //{
+            //    wordFreq.Remove(remov);
+            //}
+            //foreach (var entity in wordFreq)
+            //{
+            //    wordsCloud.Add(entity.Key);
+            //    wordsFreq.Add(entity.Value);
+            //}
+            //Image image = wc.Draw(wordsCloud, wordsFreq);
+            //image.Save(code+".png", ImageFormat.Png);
+
         }
     }
     public class Visit
