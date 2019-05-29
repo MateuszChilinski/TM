a2928e1c9c1cc518d438bffe7601fd7aa8eb335e
Mateusz Chiliński <mateusz@chilinski.eu>
2019-05-29 22:53:33 +0200

    last update

--- TMStats/Program.cs
+++ TMStats/Program.cs
@@ -23,8 +23,37 @@ namespace TMStats
             }
             return null;
         }
+        static public List<Word> LoadWords()
+        {
+            using (StreamReader r1 = new StreamReader("labels.tsv"))
+            {
+                using(StreamReader r2 = new StreamReader("vectors.tsv"))
+                {
+                    var words = new List<Word>();
+                    string name;
+                    while((name = r1.ReadLine()) != null)
+                    {
+                        Word w = new Word();
+                        w.Name = name;
+                        var vecString = r2.ReadLine();
+                        var vecArray = vecString.Split('	');
+                        double[] vec = new double[vecArray.Length];
+                        int i = 0;
+                        foreach (var scalar in vecArray)
+                        {
+                            vec[i++] = Double.Parse(scalar);
+                        }
+                        w.Vec = vec;
+                        words.Add(w);
+                    }
+                    return words;
+                }
+            }
+            return null;
+        }
         static void Main(string[] args)
         {
+            var wordsVecs = LoadWords();
             var visits = LoadJson();
             var file = File.AppendText("results.csv");
             visits.Where(x => x.Text.Length > 3).GroupBy(x => x.Code)
@@ -36,13 +65,58 @@ namespace TMStats
                 Dictionary<string, int> wordFreq = new Dictionary<string, int>();
                 foreach (var visit in visits_parsed)
                 {
-                    visit.Text = visit.Text.Replace(".", " .");
-                    visit.Text = visit.Text.Replace(":", "  :");
-                    visit.Text = visit.Text.Replace(",", " , ");
-                    visit.Text = visit.Text.Replace("(", " ( ");
-                    visit.Text = visit.Text.Replace(")", " )");
-                    visit.Text = visit.Text.Replace("  ", " ");
+                    visit.Text = visit.Text.Replace(".", "");
+                    visit.Text = visit.Text.Replace(":", "");
+                    visit.Text = visit.Text.Replace(",", "");
+                    visit.Text = visit.Text.Replace("(", "");
+                    visit.Text = visit.Text.Replace(")", "");
+                visit.Text = visit.Text.Replace("¶", " ");
+                visit.Text = visit.Text.Replace("-", " ");
+                visit.Text = visit.Text.Replace("  ", " ");
                 }
+            Dictionary<Visit, double[]> visitsEmb = new Dictionary<Visit, double[]>();
+                foreach(var visit in visits_parsed)
+                {
+                    var words = visit.Text.Split(' ');
+                    var visitVec = new double[wordsVecs[0].Vec.Length];
+                    int consideredWords = 0;
+                    foreach (var word in words)
+                    {
+                        if (wordsVecs.Count(x => x.Name == word) > 0)
+                        {
+                            consideredWords++;
+                            var vec = wordsVecs.FirstOrDefault(x => x.Name == word).Vec;
+                            for(int i = 0; i < visitVec.Length; i++)
+                            {
+                                visitVec[i] += vec[i];
+                            }
+                        }
+                    }
+                    if (consideredWords == 0) continue;
+                    for (int i = 0; i < visitVec.Length; i++)
+                    {
+                        visitVec[i] /= consideredWords;
+                    }
+                    visitsEmb.Add(visit, visitVec);
+                }
+
+            var vectors = File.AppendText("vectors_visits.tsv");
+            var labels = File.AppendText("labels_visits.tsv");
+            foreach (var entry in visitsEmb)
+            {
+                if (entry.Value[0] == 0 && entry.Value[1] == 0) continue;
+                string line = "";
+                foreach (var scalar in entry.Value)
+                    line += scalar + "\t";
+                line = line.Substring(0, line.Length - 2);
+                vectors.WriteLine(line);
+                labels.WriteLine(entry.Key.Code + "\t" + entry.Key.Text);
+            }
+            vectors.Close();
+            labels.Close();
+            var lines = System.IO.File.ReadAllLines("vectors_visits.tsv");
+            System.IO.File.WriteAllLines("vectors_visits.tsv", lines.Take(lines.Length - 1).ToArray());
+            return;
                 foreach (var visit in visits_parsed)
                 {
                     var words = visit.Text.Split(' ');
@@ -107,5 +181,10 @@ namespace TMStats
         public string Sex;
         public int Age;
         public string Code;
+    }
+    public class Word
+    {
+        public double[] Vec;
+        public string Name;
     }
 }
